9160374334d96b2c5e96689f3cceb5912f480c0f
Jack Morris <rudolphjacksonm@gmail.com>
2016-08-18 22:28:29 +0100

    Update RHEL_Setup.sh

--- RHEL_Setup.sh
+++ RHEL_Setup.sh
@@ -1,3 +1,4 @@
 # Install PowerShell on Red Hat / CentOS 7-based systems.
+#!/bin/sh
 wget https://github.com/PowerShell/PowerShell/releases/download/v6.0.0-alpha.9/powershell-6.0.0_alpha.9-1.el7.centos.x86_64.rpm
 rpm -i powershell-6.0.0_alpha.9-1.el7.centos.x86_64.rpm
